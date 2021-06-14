ce07306907d08772fc6fe15d058aa76de7b2f126
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-06-14 11:36:23 +0300

    second commit

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GameStats:
+
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stat()
+
+
+    def reset_stat(self):
+        self.ships_left = self.settings.ship_limit
+        
